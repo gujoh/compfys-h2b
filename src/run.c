c5ff62f3a28caf129c2ae90245996ea254961456
gujoh <gustafjohanson@live.com>
2024-12-11 12:21:30 +0100

    data

--- src/run.c
+++ src/run.c
@@ -3,16 +3,14 @@
 #include <stdlib.h>
 #include <gsl/gsl_rng.h>
 #include <string.h>
-
-typedef struct
-{
-    int accepted;
-    int wave; 
-} result_t; 
+#include <time.h>
+#include "tools.h"
 
 double wave(double* r1, double* r2, double alpha);
 void displace_electron(double* r, double delta, gsl_rng* k);
-result_t variational_mcmc_one_step(result_t result, double* r1, double* r2, double delta, gsl_rng* k, double alpha);
+int variational_mcmc_one_step(double* r1, double* r2, double delta, gsl_rng* k, double alpha);
+gsl_rng* get_rand(void);
+void variational_mcmc(void);
 
 int
 run(
@@ -20,28 +18,57 @@ run(
     char *argv[]
    )
 {
-    printf("Hello, world!\n");
+    variational_mcmc();
     return 0;
 }
 
-void variational_mcmc()
+void variational_mcmc(void)
 {
-    
+    double r1[] = {2, 0, 0};
+    double r2[] = {-2, 0, 0};
+    double alpha = 0.1;
+    double delta = 2;
+    gsl_rng* k = get_rand();
+    int accepted = 0;
+    int n = 1000000;
+    double temp_r1[3];
+    double temp_r2[3];
+    FILE* file = fopen("data/positions.csv", "w+");
+    if (file == NULL)
+    {
+        printf("File could not be opened.\n");
+        exit(1); 
+    }
+    for (int i = 0; i < n; i++)
+    {
+        memcpy(temp_r1, r1, sizeof(temp_r1));
+        memcpy(temp_r2, r2, sizeof(temp_r2));
+        int result = variational_mcmc_one_step(temp_r1, temp_r2, delta, k, alpha);
+        if (result == 1)
+        {   
+            accepted++;
+            memcpy(r1, temp_r1, sizeof(r1));
+            memcpy(r2, temp_r2, sizeof(r2));
+        }
+        //printf("%f,%f,%f,%f,%f,%f\n", r1[0], r1[1], r1[2], r2[0], r2[1], r2[2]);
+        fprintf(file, "%f,%f,%f,%f,%f,%f\n", r1[0], r1[1], r1[2], r2[0], r2[1], r2[2]);
+    }
+    printf("Fraction accepted: %.4f\n", (float) accepted / n);
+    fclose(file);
 }
 
-result_t variational_mcmc_one_step(result_t result, double* r1, double* r2, double delta, gsl_rng* k, double alpha)
+int variational_mcmc_one_step(double* r1, double* r2, double delta, gsl_rng* k, double alpha)
 {
-    result.accepted = 0;
+    double w1 = wave(r1, r2, alpha);
     displace_electron(r1, delta, k);
     displace_electron(r2, delta, k);
     double w2 = wave(r1, r2, alpha);
     double r = gsl_rng_uniform(k);
-    if (r < w2 / result.wave)
+    if (r < w2 / w1)
     {
-        result.wave = w2; 
-        result.accepted = 1; 
+        return 1;  
     }
-    return result; 
+    return 0; 
 }
 
 void displace_electron(double* r, double delta, gsl_rng* k)
@@ -59,4 +86,15 @@ double wave(double* r1, double* r2, double alpha)
     double r12_len = distance_between_vectors(r1, r2, 3);
     return exp(- 2 * r1_len) * exp(- 2 * r2_len) * 
         exp(r12_len / (2 + 2 * alpha * r12_len));
+}
+
+gsl_rng* get_rand(void){
+    const gsl_rng_type* T;
+    gsl_rng* r;
+    gsl_rng_env_setup();
+    T = gsl_rng_default;
+    r = gsl_rng_alloc(T);
+    time_t seed = time(NULL);
+    gsl_rng_set(r, seed);
+    return r;
 }
