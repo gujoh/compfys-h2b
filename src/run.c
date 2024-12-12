1a04aff318c29bb8185988dc0fa4b5b1c8b76d2b
gujoh <gustafjohanson@live.com>
2024-12-12 10:44:08 +0100

    wooo task2

--- src/run.c
+++ src/run.c
@@ -60,8 +60,8 @@ void task1(void)
 
 void task2(void)
 {
-    double r1[] = {42, 42, 42};
-    double r2[] = {-42, -42, -42};
+    double r1[] = {75, 0, 0};
+    double r2[] = {0, -75, 0};
     double alpha = 0.1;
     double delta = 2; 
     int n = 5000;
